5514f0fe37eb396c8827d5dd5e0de7317fdd328b
mmyakisv <mmyakisv@yandex.ru>
2023-06-17 12:17:28 +0300

    Доработка Задание №3

--- sandbox/src/main/java/ru/stqa/pftester/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pftester/sandbox/MyFirstProgram.java
@@ -15,18 +15,10 @@ public class MyFirstProgram {
 
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-    Point p = new Point();
-    p.p1 =
-    p.p2 =
-    System.out.println("Расстояние между двумя точками " + p.p1 + " и " + p.p2 + " = " + distance(p));
-
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
 
-  public static double distance(Point p) {
-    return (p.p1 - p.p2);
   }
-}
--- sandbox/src/test/java/ru/stqa/pftester/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pftester/sandbox/PointTests.java
@@ -6,9 +6,21 @@ import org.testng.annotations.Test;
 public class PointTests {
 
     @Test
-    public void testDistance() {
+    public void testDistance1() {
         Point p1 = new Point(2, 9);
         Point p2 = new Point(18, 25);
         Assert.assertEquals(p1.distance(p2), 22.627416997969522);
+    }
+    @Test
+    public void testDistance2() {
+        Point p1 = new Point(8, 9);
+        Point p2 = new Point(4, 6);
+        Assert.assertEquals(p1.distance(p2), 5.0);
+    }
+    @Test
+    public void testDistance3() {
+        Point p1 = new Point(-1, -1);
+        Point p2 = new Point(4, 6);
+        Assert.assertEquals(p1.distance(p2), 8.602325267042627);
     }
 }
