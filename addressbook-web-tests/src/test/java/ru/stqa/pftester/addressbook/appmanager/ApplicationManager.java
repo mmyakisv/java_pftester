ea2f4dc6310b0fe4dc6947782f1bda1a6b6b0c1e
mmyakisv <mmyakisv@yandex.ru>
2023-06-18 21:43:23 +0300

    Задание №5:  Создание общего класса BaseHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pftester/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pftester/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,6 @@
 package ru.stqa.pftester.addressbook.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -30,14 +29,9 @@ public class ApplicationManager {
         sessionHelper.login("admin", "secret");
     }
 
-
-
-
-
     public void stop() {
         wd.quit();
     }
-
     public boolean isElementPresent(By by) {
         try {
             wd.findElement(by);
@@ -47,14 +41,6 @@ public class ApplicationManager {
         }
     }
 
-    public boolean isAlertPresent() {
-        try {
-            wd.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
 
     public GroupHelper getGroupHelper() {
         return groupHelper;
