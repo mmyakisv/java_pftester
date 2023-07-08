d34a752025de037f2d2f8a14a7fffa613254bc6c
mmyakisv <mmyakisv@yandex.ru>
2023-07-08 13:28:11 +0300

    Сортировка списков

--- addressbook-web-tests/src/test/java/ru/stqa/pftester/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pftester/addressbook/model/GroupData.java
@@ -8,8 +8,10 @@ public class GroupData {
     private final String header;
     private final String footer;
 
+
+
     public GroupData(String name, String header, String footer) {
-        this.id = 0;
+        this.id = Integer.MAX_VALUE;
         this.name = name;
         this.header = header;
         this.footer = footer;
@@ -49,19 +51,19 @@ public class GroupData {
                 ", name='" + name + '\'' +
                 '}';
     }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            GroupData groupData = (GroupData) o;
-            return id == groupData.id && Objects.equals(name, groupData.name);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(id, name);
-        }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupData groupData = (GroupData) o;
+        return Objects.equals(name, groupData.name);
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+}
 
 
