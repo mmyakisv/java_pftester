3e27f6d365143a035acc4a6d1023a8a1a35f5e55
mmyakisv <mmyakisv@yandex.ru>
2023-07-13 21:01:34 +0300

    Задание №8: Доработка

--- addressbook-web-tests/src/test/java/ru/stqa/pftester/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pftester/addressbook/tests/ContactCreationTests.java
@@ -10,20 +10,21 @@ import java.util.List;
 
 public class ContactCreationTests extends TestBase{
 
-
-
-
-
     @Test
     public void testContactCreation() throws Exception {
-
+        if (!app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", "null", "null"));
+        }
+        app.getNavigationHelper().gotoGroupPage();
+        GroupData group = new GroupData("test2", "null", "null");
+        app.getGroupHelper().createGroup(group);
+        app.getContactHelper().returnToContactPage();
 
         List<ContactData> before = app.getContactHelper().getContactList();
         ContactData contact = new ContactData("Avatar", "Avatarovich", "78005678930", "tester@gmail.com", "test1");
         app.getContactHelper().createContact(contact);
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() + 1);
-
 
         before.add(contact);
         Comparator<? super ContactData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
