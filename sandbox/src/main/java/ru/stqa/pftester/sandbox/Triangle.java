273d1746fab1ec5dbb2b3933b0dbaa6bad56604d
mmyakisv <mmyakisv@yandex.ru>
2023-10-08 22:40:37 +0300

    Задание №3: Потренироваться использовать ветвление и исключения

--- sandbox/src/main/java/ru/stqa/pftester/sandbox/Triangle.java
+++ sandbox/src/main/java/ru/stqa/pftester/sandbox/Triangle.java
@@ -5,11 +5,22 @@ public class Triangle {
     public double b;
     public double c;
 
+
+
+
     public Triangle(double a, double b, double c) {
+
 
         this.a = a;
         this.b = b;
         this.c = c;
+
+        if (a <0 || b<0 || c<0) {
+            throw new IllegalArgumentException("Triangle side should be non-negative");
+        }
+        if (a + b < c || b + c < a || a + c < b) {
+            throw new IllegalArgumentException("The sum of any two sides must not be less than the third");
+        }
     }
 
 
@@ -19,8 +30,8 @@ public class Triangle {
 
     public double square() {
         return Math.sqrt((perimeter() / 2 * (perimeter() / 2 - this.a) * (perimeter() / 2 - this.b) * (perimeter() / 2 - this.c)));
-
-
     }
+
+
 }
 
--- sandbox/src/test/java/ru/stqa/pftester/sandbox/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/pftester/sandbox/TriangleTests.java
@@ -1,7 +1,9 @@
 package ru.stqa.pftester.sandbox;
 
+import org.junit.jupiter.api.Assertions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
 
 public class TriangleTests {
 
@@ -14,8 +16,8 @@ public class TriangleTests {
 
     @Test
     public void perimeter1() {
-        Triangle p = new Triangle(4, 4, 10);
-        Assert.assertEquals(p.perimeter(), 18.0);
+        Triangle p = new Triangle(4, 4, 7);
+        Assert.assertEquals(p.perimeter(), 15.0);
     }
 
     @Test
@@ -34,5 +36,42 @@ public class TriangleTests {
     public void square1() {
         Triangle s = new Triangle(8, 8, 8);
         Assert.assertEquals(s.square(), 27.712812921102035);
+    }
+
+    @Test
+    void cannotCreateTriangleWithNegativeSide1() {
+        try {
+            new Triangle(-4, 4, 10);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Triangle side should be non-negative");
+        }
+    }
+    @Test
+    void cannotCreateTriangleWithNegativeSide2() {
+        try {
+            new Triangle(4, -4, 10);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Triangle side should be non-negative");
+        }
+    }
+    @Test
+    void cannotCreateTriangleWithNegativeSide3() {
+        try {
+            new Triangle(4, 4, -10);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Triangle side should be non-negative");
+        }
+    }
+    @Test
+    void cannotCreateTriangleWithSumBothSides() {
+        try {
+            new Triangle(13, 7, 21);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            System.out.println("The sum of any two sides must not be less than the third");
+        }
     }
 }
