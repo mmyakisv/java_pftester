22a3ebd1665d48342260b0518d9b1a56adc9fd6f
mmyakisv <mmyakisv@yandex.ru>
2023-10-15 15:24:37 +0300

    Базовый класс для тестов

--- addressbook-web-tests/src/test/java/GroupRemovalTests.java
+++ addressbook-web-tests/src/test/java/GroupRemovalTests.java
@@ -1,64 +1,16 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
 
-public class GroupRemovalTests {
-    private WebDriver driver;
+public class GroupRemovalTests extends TestBase {
 
-    @BeforeEach
-    public void setUp() {
-        var service = ChromeDriverService.createDefaultService();
-        service.setExecutable("c:/windows/system32/chromedriver.exe");
-        driver = new ChromeDriver(service);
-        driver.get("http://localhost/addressbook/");
-        driver.manage().window().setSize(new Dimension(1918, 1040));
-        driver.findElement(By.name("user")).click();
-        driver.findElement(By.name("user")).sendKeys("admin");
-        driver.findElement(By.name("pass")).click();
-        driver.findElement(By.name("pass")).sendKeys("secret");
-        driver.findElement(By.cssSelector("input:nth-child(7)")).click();
-    }
-
-    @AfterEach
-    public void tearDown() {
-        driver.findElement(By.linkText("Logout")).click();
-        driver.quit();
-    }
 
     @Test
     public void canRemoveGroup() {
-        if (!isElementPresent(By.name("new"))) {
-            driver.findElement(By.linkText("groups")).click();
+        openGroupsPage();
+        if (!isGroupPresent()) {
+            createGroup();
         }
-        if (!isElementPresent(By.name("selected[]"))) {
-            driver.findElement(By.name("new")).click();
-            driver.findElement(By.name("group_name")).click();
-            driver.findElement(By.name("group_name")).sendKeys("test1");
-            driver.findElement(By.name("group_header")).click();
-            driver.findElement(By.name("group_header")).sendKeys("group header");
-            driver.findElement(By.name("group_footer")).click();
-            driver.findElement(By.name("group_footer")).click();
-            driver.findElement(By.name("group_footer")).sendKeys("group footer");
-            driver.findElement(By.name("submit")).click();
-            driver.findElement(By.linkText("group page")).click();
-        }
-        driver.findElement(By.name("selected[]")).click();
-        driver.findElement(By.name("delete")).click();
-        driver.findElement(By.linkText("group page")).click();
+        removeGroup();
     }
 
-    private boolean isElementPresent(By locator) {
-        try {
-            driver.findElement(locator);
-            return true;
-        } catch (NoSuchElementException exception) {
-            return false;
-        }
-    }
 }
+
