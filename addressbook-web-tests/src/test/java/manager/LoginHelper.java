3a53f767d3bac63b16807c9751d0d356d7bc5f16
mmyakisv <mmyakisv@yandex.ru>
2023-10-18 22:18:43 +0300

    Задание №6: Реализовать двухуровневую архитектуру тестового набора

--- addressbook-web-tests/src/test/java/manager/LoginHelper.java
+++ addressbook-web-tests/src/test/java/manager/LoginHelper.java
@@ -2,20 +2,16 @@ package manager;
 
 import org.openqa.selenium.By;
 
-public class LoginHelper {
-    private final ApplicationManager manager;
+public class LoginHelper extends HelperBase {
 
     public LoginHelper(ApplicationManager manager) {
 
-        this.manager = manager;
+        super(manager);
     }
+
     void login(String user, String password) {
-        manager.driver.findElement(By.id("LoginForm")).click();
-        manager.driver.findElement(By.name("user")).click();
-        manager.driver.findElement(By.name("user")).sendKeys(user);
-        manager.driver.findElement(By.name("pass")).click();
-        manager.driver.findElement(By.name("pass")).sendKeys(password);
-        manager.driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
-        manager.driver.findElement(By.id("header")).click();
+        type(By.name("user"), user);
+        type(By.name("pass"), password);
+        click(By.xpath("//input[@value=\'Login\']"));
     }
 }
